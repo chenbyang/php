78665f06172bb87c726cb7f90677db84dfb6ab65
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-04-12 10:13:38 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -3,8 +3,8 @@
     <body>
         <?php echo "Hello world!"; ?>//在網頁上顯示"Hello world!"; 
         <h1>html</h1>//顯示一個標題文字叫做html
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?>//在網頁上顯示"Hello world!"; 
+        <h1>html</h1>//顯示一個標題文字叫做html
+        <?php echo "Hello world!"; ?>//在網頁上顯示"Hello world!"; 
     </body>
 </html>
