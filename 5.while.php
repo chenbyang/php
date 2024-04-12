313b4143e74c91bfa733d9a0c609f7969df83243
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-12 09:59:55 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,10 +1,10 @@
 <?php
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //創建一個建立資料庫連結的變數$conn
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");
+   $result=mysqli_query($conn, "select * from user"); //創建一個查詢資料庫指令select * from user的變數$result
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   while ($row=mysqli_fetch_array($result)) {
-     echo $row["id"]." ".$row["pwd"]."<br>";
+   while ($row=mysqli_fetch_array($result)) { //一個while迴圈 無法抓取到資料後就會停止
+     echo $row["id"]." ".$row["pwd"]."<br>"; //顯示 immust資料庫中id 和 pwd的值 
    } 
 ?>
