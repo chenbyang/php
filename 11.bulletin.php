a68ceadf2a70d5caab6764534aa4c088ddc1c1e0
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-12 11:54:47 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -1,17 +1,18 @@
 <?php
-    error_reporting(0);
-    session_start();
-    if (!$_SESSION["id"]) {
-        echo "請先登入";
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    error_reporting(0); //不要顯示錯誤報告
+    session_start(); //開啟一個SESSION
+    if (!$_SESSION["id"]) { //假如沒有SESSION["id"]
+        echo "請先登入"; 網頁顯示 請先登入
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>"; //3秒後自動跳回2.login.html
     }
     else{
         echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
-        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-        $result=mysqli_query($conn, "select * from bulletin");
-        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
-        while ($row=mysqli_fetch_array($result)){
-            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
+        # 顯示 歡迎 XXSESSION + 登出的超連結點了之後回跳轉到 12.logout.php +管理使用者的超連結點選之後會跳到 18.user.php + 新增佈告的超連結點選之後會跳到 22.bulletin_add_form.php
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //創建一個建立資料庫連結的變數$conn
+        $result=mysqli_query($conn, "select * from bulletin"); //創建一個查詢資料庫指令 select * from bulletin 的變數$result
+        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";  //顯示一個表格邊框像素為2，顯示表格每格按照順序顯示佈告編號到發佈時間
+        while ($row=mysqli_fetch_array($result)){ //創建一個while迴圈無法抓取到資料後就會停止
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a>
             <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
             echo $row["bid"];
             echo "</td><td>";
