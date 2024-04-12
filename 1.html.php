59a1d8adb0776c6b00a32193fd02dcf044f5a703
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-12 11:18:19 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,7 +1,7 @@
 <html>
     <head><title>html+php</title></head> // 顯示網站的標題  
     <body>
-        <?php echo "Hello world!"; ?> //在網頁上print Hello world!
+        <?php echo "Hello world!"; ?> //在網頁上顯示 Hello world!
         <h1>html</h1> //顯示標題大小最大的 html
         <?php echo "Hello world!"; ?> 
         <h1>html</h1>
