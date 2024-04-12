7f11ad999b84cfa55002e05d30a564b690b8821f
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-12 09:48:54 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
-    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))
-        echo "登入成功";
+    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234")) //假如2.login.html傳過來的id和pwd是john跟john1234
+        echo "登入成功"; //是true就顯示登入成功
     else
-        echo "登入失敗";
+        echo "登入失敗"; //是false就顯示登入失敗
 ?>
