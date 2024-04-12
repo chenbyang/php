356a9c3c2e906e08cd92b84c31a3dcfc43838bc5
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-04-12 11:59:25 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,17 +1,17 @@
 <?php
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");//創建一個建立資料庫連結的變數$conn
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");
+   $result=mysqli_query($conn, "select * from user");//創建一個查詢資料庫指令"select * from user"的變數$result
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   $login=FALSE;
-   while ($row=mysqli_fetch_array($result)) {
+   $login=FALSE;//將$login設定為布林值FALSE
+   while ($row=mysqli_fetch_array($result)) { //創建一個while迴圈無法抓取到資料後就會停止
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
-       $login=TRUE;
+       $login=TRUE; //如果結果為True會將$login的布林值設定為TRUE
      }
    } 
    if ($login==TRUE)
-     echo "登入成功";
+     echo "登入成功"; //$LOGIN變數為TRUE會顯示登入成功在網頁上
   else
-     echo "帳號/密碼 錯誤";
+     echo "帳號/密碼 錯誤"; //$LOGIN變數為FALSE會顯示帳號&密碼錯誤在網頁上
 ?>
