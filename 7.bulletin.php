8a3af3cd1b61c5facd5423d1ecb70795d1f9dd3a
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-12 11:34:29 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,7 +1,7 @@
 <?php
     error_reporting(0); //禁用PHP錯誤報告
     $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //創建一個建立資料庫連結的變數$conn
-    $result=mysqli_query($conn, "select * from bulletin"); //創建一個查詢資料庫指令select * from user的變數$result
+    $result=mysqli_query($conn, "select * from bulletin"); //創建一個查詢資料庫指令select * from bulletin的變數$result
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>"; //顯示一個表格邊框像素為2，顯示表格每格按照順序顯示佈告編號到發佈時間
     while ($row=mysqli_fetch_array($result)){ //一個while迴圈 無法抓取到資料後就會停止
         echo "<tr><td>"; //定義一個表格行 定義一個表格單元
