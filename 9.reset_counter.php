befdef513c65017bd9833f2f3937d4cb6138411c
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-04-18 23:53:01 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start();//開始一個SESSION
+    unset($_SESSION["counter"]);//清除counter的SESSION
+    echo "counter重置成功....";//在網頁上顯示 counter重置成功....
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";//在2秒後會自動跳到 8.counter.php
 
 ?>
