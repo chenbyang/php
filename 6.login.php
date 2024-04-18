397e42fa6658a850058c206ef84cfdba5aa74b48
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-04-18 22:30:45 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -4,7 +4,7 @@
    #mysqli_query() 從資料庫查詢資料
    $result=mysqli_query($conn, "select * from user"); //創建一個查詢資料庫指令select * from user的變數$result
    #mysqli_fetch_array() //從查詢出來的資料一筆一筆抓出來
-   $login=FALSE; //將$login設定為布林值FALSE
+   $login=FALSE; //將$login變數設定為布林值FALSE
    while ($row=mysqli_fetch_array($result)) { //創建一個while迴圈無法抓取到資料後就會停止
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) { //假如輸入的id和資料庫的id一樣 && 輸入的pwd和資料庫的pwd一樣
        $login=TRUE; //如果結果為True會將$login的布林值設定為TRUE
