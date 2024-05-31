070bab299684763e423f31be18021ef952e53732
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-05-31 10:19:09 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    unset($_SESSION["id"]);
+    session_start();//啟動繪畫
+    unset($_SESSION["id"]);//刪除名為"id" 的會話變數。
     echo "登出成功....";
     echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 
