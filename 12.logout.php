4ba6a9a950f0fce2815185719f5176fa09e02542
bobo4545 <49523109+bobo4545@users.noreply.github.com>
2024-05-31 10:35:42 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["id"]);
-    echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    session_start(); //啟動Session
+    unset($_SESSION["id"]); //刪除一個叫做id的Session
+    echo "登出成功...."; //網頁上印出登出成功....
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>"; //三秒後跳轉到2.login.html
 
 ?>
