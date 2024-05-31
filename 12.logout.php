0800fc95ed0c08d6c32c41e9fddadad782966f68
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-05-31 10:22:35 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();//啟動繪畫
     unset($_SESSION["id"]);//刪除名為"id" 的會話變數。
-    echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    echo "登出成功....";//映出"登出成功...."到網頁上。
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";//3秒後跳轉到url=2.login.html網頁
 
 ?>
