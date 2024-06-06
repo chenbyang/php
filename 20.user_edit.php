fcd40a0f32fca5fac2b0b12566719e7b58c9a356
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-06-06 20:17:33 +0800

    Update 20.user_edit.php

--- 20.user_edit.php
+++ 20.user_edit.php
@@ -1,10 +1,10 @@
 <?php
 
-    error_reporting(0); // 關閉錯誤報告顯示
+    error_reporting(0); // 關閉錯誤的報告顯示
     session_start(); // 啟動Session
     if (!$_SESSION["id"]) { //如果Session沒有id
         echo "請登入帳號"; //網頁印出請登入帳號
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";  // 三秒後跳轉網頁到2.login.html
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";  // 三秒後跳轉到網頁2.login.html
     }
     else{    //有id
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");  //資料庫連線
