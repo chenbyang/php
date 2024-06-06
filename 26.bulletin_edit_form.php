2e0a75308db11a48422eb1227c3e49bc94e18eb0
chenbyang <166775069+chenbyang@users.noreply.github.com>
2024-06-06 20:40:06 +0800

    Update 26.bulletin_edit_form.php

--- 26.bulletin_edit_form.php
+++ 26.bulletin_edit_form.php
@@ -3,22 +3,22 @@
     session_start(); // 啟動Session
     if (!$_SESSION["id"]) {  //如果Session沒有id
         echo "please login first";  //網頁印出請登入帳號  
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>"; // 三秒後跳轉網頁到2.login.html
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>"; // 三秒後跳轉到網頁2.login.html
     }
     else{
         
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); //資料庫連線
-        $result=mysqli_query($conn, "select * from bulletin where bid={$_GET["bid"]}"); //資料庫查詢指令
-        $row=mysqli_fetch_array($result); //資料庫查詢指令
-        $checked1=""; //變數1設定空字串
-        $checked2=""; //變數2設定空字串
-        $checked3=""; //變數3設定空字串
+        $result=mysqli_query($conn, "select * from bulletin where bid={$_GET["bid"]}"); //資料庫指令查詢
+        $row=mysqli_fetch_array($result); //資料庫指令查詢
+        $checked1=""; //設定變數1的空字串
+        $checked2=""; //設定變數2的空字串
+        $checked3=""; //設定變數3的空字串
         if ($row['type']==1) //如果type 1被勾選
-            $checked1="checked"; //設定變數1字串內容
+            $checked1="checked"; //設定字串變數1內容
         if ($row['type']==2) //如果type 2被勾選
-            $checked2="checked"; //設定變數2字串內容
+            $checked2="checked"; //設定字串變數2內容
         if ($row['type']==3) //如果type 3被勾選
-            $checked3="checked"; //設定變數3字串內容
+            $checked3="checked"; //設定字串變數3內容
         echo "
         <html>
             <head><title>新增佈告</title></head> //網頁標頭
@@ -32,7 +32,7 @@
                             <input type=radio name=type value=3 {$checked3}>徵才資訊<br>
                     發布時間：<input type=date name=time value={$row['time']}><p></p>
                     <input type=submit value=修改佈告> <input type=reset value=清除>
-                </form> //表單結尾
+                </form> //表單的結尾
             </body>
         </html>
         ";
